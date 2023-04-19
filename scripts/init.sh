edda65462bcd209c3c061546435b3404e87f7eee
debojyoti452 <debojyoti452@gmail.com>
2023-04-20 01:09:27 +0530

    removed-scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -26,4 +26,4 @@
 #  */
 #
 
-flutter clean &&  flutter pub get &&  flutter pub run build_runner build --delete-conflicting-outputs
+flutter clean && flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
